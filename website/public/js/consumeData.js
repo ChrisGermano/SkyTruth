4931360ef880fd76d890b640f87a16d3b2279097
Chris Germano <chris.eg.germano@gmail.com>
2019-12-02 18:31:11 -0500

    UI layout, tweaks, data mocking, report download, etc

--- website/public/js/consumeData.js
+++ website/public/js/consumeData.js
@@ -12,13 +12,33 @@ let spill = {
 }
 */
 
-let allSpillData = [];
-
 $.get('API endpoint', function(data) {
   // Assuming data is a JSON formatted as expected
   data.forEach(function(spillData) {
     allSpillData.push(spillData);
   })
 });
+
+let consumeSpillData = function() {
+  let allSpillData = [];
+
+  // Stubbed
+  [
+    {"uniqueID": "BIG BAD OIL SPILL", "latitude": 36.1699, "longitude": 115.1398},
+    {"uniqueID": "Little Teeny Oil Spill", "latitude": 40.7128, "longitude": 74.0060},
+    {"uniqueID": "Ocean Oil Spill", "latitude": 0, "longitude": 0}
+  ]
+
+
+  /*
+  $.get('API endpoint', function(data) {
+    // Assuming data is a JSON formatted as expected
+    data.forEach(function(spillData) {
+      allSpillData.push(spillData);
+    })
+  });
+  */
+  return allSpillData;
+}
 
 //More TBD once I get new data
