554336d1d5ec08112eb3c677472d89304cca56f6
Chris Germano <chris.eg.germano@gmail.com>
2019-12-02 14:25:48 -0500

    Setup views and frontend

--- index.js
+++ index.js
@@ -3,8 +3,29 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
+const engines = require('consolidate');
 const port = process.env.PORT || 9001;
 
 const storedConfig = require('./config.js');
 
 const config = {}; //External variables not for Git, credentials etc
+
+app.set('views', __dirname + '/views');
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index.html", { title: "Home"});
+});
+
+app.listen(port, () => {
+  console.log('Listening on ' + port)
+})
