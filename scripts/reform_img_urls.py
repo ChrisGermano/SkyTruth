d1d5cdae716c821874974c9ae872d07a6565dd49
charleygrossman <charleygrossman@gmail.com>
2019-12-02 19:06:32 -0600

    1 hr left

--- scripts/reform_img_urls.py
+++ scripts/reform_img_urls.py
@@ -1,9 +1,5 @@
-from uuid import uuid4
-import json
 import io
 import boto3
-import requests
-import envsettings
 
 BUCKET_NAME = 'skytruthdata'
 
@@ -13,14 +9,10 @@ BUCKET_CSV = 'Training_Sample/Oil_Slick_Database.csv'
 # Put image objects inside this folder
 BUCKET_OUTPUT_PREFIX = 'Training_Images/'
 
+FILE_PREFIX = ''
+
 
 def reform_img_urls():
-    # s3 = boto3.resource(
-    #     's3',
-    #     aws_access_key_id=envsettings.AWS_ACCESS_KEY_ID,
-    #     aws_secret_access_key=envsettings.AWS_SECRET_ACCESS_KEY
-    # )
-
     s3 = boto3.client('s3')
 
     obj = s3.get_object(Bucket=BUCKET_NAME, Key=BUCKET_CSV)
@@ -29,15 +21,8 @@ def reform_img_urls():
     file = io.StringIO(data)
     for line in file.readlines()[1:2]:
         line = line.strip().split(',')
-        print(line)
-
-
-    # file = obj.get()['Body'].read().decode('utf-8')
-    #
-    # for line in file.readlines()[:100]:
-    #     print(line)
+        img = line[0]
 
 
 if __name__ == '__main__':
-
     reform_img_urls()
--- skytruth-api/app.py
+++ skytruth-api/app.py
@@ -1,8 +1,7 @@
-from uuid import uuid4
 import boto3
-import requests
+import json
 from chalice import Chalice
-import envsettings
+
 
 app = Chalice(app_name='skytruth-api')
 
@@ -14,52 +13,21 @@ BUCKET_CSV_PREFIX = 'Training_Sample/'
 # Put image objects inside this folder
 BUCKET_OUTPUT_PREFIX = 'Training_Images/'
 
+TABLE_NAME = "candidate_ships"
+
+
+# TODO
+@app.route('/candidates', methods=['GET'])
+def candidates():
+    client = boto3.client('dynamodb')
+
+    table = client.Table(TABLE_NAME)
+
+    data = table.query( KeyConditionExpression=Key('image_id'))
+
+    return json.dumps(data['Items'])
+
 
 @app.route('/', methods=['GET'])
 def index():
     return {'hello': 'world'}
-
-
-# TODO: Returns a list of most likely lat/long objects
-@app.route('/candidates', methods=['GET'])
-def candidates():
-    return {
-        "latitude": 36.1699,
-        "longitude": 115.1398,
-        "id": uuid4()
-    }
-
-
-@app.route('/reform-img-urls', methods=['GET'])
-def reform_img_urls():
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket(BUCKET_NAME)
-
-    for objs in bucket.objects.filter(Prefix=BUCKET_CSV_PREFIX):
-        print(objs)
-
-
-
-
-
-
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
